9544f5944cd4ea54922c48bd528c5536647f2aab
father-lox <kot.mi.mi.992@gmail.com>
2021-07-14 15:21:12 +0300

    Initialized repository

---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+const {src, dest, watch, parallel} = require("gulp");
+
+const browserSync = require('browser-sync').create();
+const less = require('gulp-less');
+const ttf2woff = require("gulp-ttf2woff");
+const ttf2woff2 = require("gulp-ttf2woff2");
+
+function LESSpreprocessing() {
+  return src('build/less/**/*.less')
+    .pipe(less())
+    .pipe(dest('dist/css'))
+    .pipe(browserSync.stream());;
+};
+
+function HTMLpreprocessing() {
+    return src("build/*.html")
+        .pipe(dest('dist'))
+        .pipe(browserSync.stream());
+}
+
+function image() {
+    return src("build/img/**/*")
+        .pipe(dest('dist/img/'))
+}
+
+function fontConverter() {
+    // src("build/fonts/*.ttf")
+    //     .pipe(ttf2woff())
+    //     .pipe(dest("dist/fonts/"));
+    return src("build/fonts/*.ttf")
+        .pipe(dest("dist/fonts/"));
+    // return src("build/fonts/*.ttf")
+    //     .pipe(ttf2woff2())
+    //     .pipe(dest("dist/fonts/"));
+}
+
+function browser() {
+    browserSync.init({
+        server: {
+            baseDir: "dist"
+        }
+    });
+}
+
+function watching() {
+    watch(["build/less/**/*.less"], LESSpreprocessing);
+    watch(["build/*.html"], HTMLpreprocessing);
+    watch(["build/img/**/*"], image);
+    watch("dist/*.html").on('change', browserSync.reload);
+    watch(["build/fonts/*"], fontConverter);
+}
+
+exports.image = image;
+exports.LESSpreprocessing = LESSpreprocessing;
+exports.HTMLpreprocessing = HTMLpreprocessing;
+exports.watching = watching;
+exports.browser = browser;
+exports.fontConverter = fontConverter;
+
+exports.default = parallel(browser, watching);
