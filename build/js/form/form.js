5bd989184058f0b6c0e7b31269dc62a0fc9d910a
father-lox <kot.mi.mi.992@gmail.com>
2021-07-25 13:16:09 +0300

    Fixed promt resizing

--- build/js/form/form.js
+++ build/js/form/form.js
@@ -4,4 +4,16 @@ for(let i = 0; i < inputs.length; i++) {
     inputs[i].addEventListener("input", function() {
         this.value = this.value.toUpperCase();
     });
+
+    inputs[i].addEventListener("focus", function() {
+        if(this.classList.length === 1) {
+            this.classList.add("field-input__input_hidden-promt");
+        }
+    });
+
+    inputs[i].addEventListener("blur", function() {
+        if(this.value.length === 0) {
+            this.classList.remove("field-input__input_hidden-promt");
+        }
+    });
 }
